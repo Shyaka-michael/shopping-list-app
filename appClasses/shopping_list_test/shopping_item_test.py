ef6bfc8b0c220a69454108e7bd7b01ebb16cb889
shyaka <shyaka>
2017-08-15 03:45:47 -0700

    used pylint to clean up code

--- appClasses/shopping_list_test/shopping_item_test.py
+++ appClasses/shopping_list_test/shopping_item_test.py
@@ -1,12 +1,23 @@
+"""
+module for testing item class
+"""
+
 import unittest
 from ..shopping_classes.shopping_item import Item
 
 
-class Test_shopping_item(unittest.TestCase):
+class TestShoppingItem(unittest.TestCase):
+    """
+    main class to test item class name and quantity
+    """
 
     def setUp(self):
         self.item_one = Item("soap", 3)
 
     def test_item(self):
+        """
+            methods for testing item class name and quantity
+        """
         print(self.item_one)
         self.assertEqual(self.item_one.name, "soap", msg='item not found')
+        self.assertEqual(self.item_one.quantity, 3, msg='quantity not found')

571447fa724fb4f9846a628f46e9b1640f0ea8d3
shyaka <shyaka>
2017-08-17 02:54:02 -0700

    added changes to view file

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
+@app.route('/login')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Miguel'}
+    return render_template('login.html', title='Home', user=user)
+
+
+@app.route('/register')
+def sign_up():
+    #user = {'nickname': 'Miguel'}
+    return render_template('register.html', title='sing up')
